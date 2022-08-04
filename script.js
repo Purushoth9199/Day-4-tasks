09a38fb8e2283e3da008a6830ff352c062987392
Purushoth9199 <108861486+Purushoth9199@users.noreply.github.com>
2022-08-04 21:10:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var request=new XMLHttpRequest();
+request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
+request.send();
+request.onload=function(){
+    var result=JSON.parse(request.response);
+    console.log(result);
+
+    var res=result.map((ele)=>ele.flag);
+    console.log(res);
+}
